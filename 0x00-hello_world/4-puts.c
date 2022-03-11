45e1ce51e7b895b4357941b328a9e20c7a1c8241
Nellyniamson <niamsonnelly@gmail.com>
2022-03-11 10:48:47 -0800

    print the sentence with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - entry point
- *
- * Return: Always (success)
+ * Return: always return 0 (success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
