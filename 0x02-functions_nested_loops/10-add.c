d44aa4318418fc3b191ac2c7723e9e02e90bcbe3
Nellyniamson <niamsonnelly@gmail.com>
2022-03-15 21:02:15 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -entry
+ * @one: first rntry
+ * @two: second rntr
+ * Return: result
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
