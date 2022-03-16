42077fedfeb7777b1cb5a3834e0b91a5c42955e0
Nellyniamson <niamsonnelly@gmail.com>
2022-03-15 18:13:57 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+
+	_putchar('\n'');
+	return (0);
+}
