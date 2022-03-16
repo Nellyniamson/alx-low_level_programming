be7677fd06c26d913f9f5ef59716f21306e836cd
Nellyniamson <niamsonnelly@gmail.com>
2022-03-15 19:00:15 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * _islower - short description
+ * @c: contains value
+ * Return: 1 if it is in lowercase or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
