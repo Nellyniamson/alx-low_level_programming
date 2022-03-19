7255a269830c1eae83a06b5d9f50efb905694dad
Nellyniamson <niamsonnelly@gmail.com>
2022-03-18 19:45:33 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int p = 100;
+
+	i = 1;
+	while (i <= p)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i < p)
+				printf("Buzz");
+			else
+			{
+				printf("Buzz");
+			}
+		}
+
+		else
+		{
+			printf("%i", i);
+		}
+
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
