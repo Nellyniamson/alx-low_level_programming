351d1d6e5f2e1776537028e63bbe25f1098888da
Nellyniamson <niamsonnelly@gmail.com>
2022-03-18 18:00:39 -0700

    commit numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int in, ro;
+
+	for (ro = 0; ro < 10; i++)
+	{
+		for (i = 0, i <= 14, i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	{
+
