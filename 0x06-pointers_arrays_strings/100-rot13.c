1b70f67745eeefeb3e45b02d0925c4b69e2f2281
Nellyniamson <niamsonnelly@gmail.com>
2022-03-25 16:52:37 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string usng rot13
+ * @s: input string
+ * Return: the pointer to dest
+ */
+
+char *rot13(char *s)
+{
+	int count = 0, i;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijiklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alph[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
