6ba48aa287bf2c2627ce9be30cf27c70bcb9141c
Nellyniamson <niamsonnelly@gmail.com>
2022-04-05 06:17:56 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strdup - duplicate
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i, end;
+	char *array;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (end = 0; end <= *str; end++)
+	{
+	}
+
+	end += 1;
+	array = malloc(sizeof(char) * end);
+
+	for (i = 0; i < end; i++)
+	{
+		array[i] = str[i];
+	}
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	return (array);
+}
