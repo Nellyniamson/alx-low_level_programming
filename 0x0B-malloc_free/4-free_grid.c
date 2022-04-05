c6f7cc76ff66d5c30c83ea9b51e67f56c2bcdad6
Nellyniamson <niamsonnelly@gmail.com>
2022-04-05 15:37:00 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.c"
+/**
+ * free_grid - allocates a grid
+ * @grid: takes in width of grind
+ * @height: height of grid
+ * Return: free grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
