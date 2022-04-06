3dbee0bf6bcaa28af110d7dbd619ca77af77eeef
Nellyniamson <niamsonnelly@gmail.com>
2022-04-06 16:45:24 -0700

    args 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry
+ * @argc: arguments
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i];
+		i++;
+	}
+
+	return (0);
+}
