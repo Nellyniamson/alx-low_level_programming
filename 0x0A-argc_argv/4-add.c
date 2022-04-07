855af7a294254eaf8122aae64119279bd3be1a6e
Nellyniamson <niamsonnelly@gmail.com>
2022-04-06 17:06:15 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry
+ * @argc: arguments
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+	{
+		return (0);
+	}
+
+	for (i = 1, i < argc; i++;)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
